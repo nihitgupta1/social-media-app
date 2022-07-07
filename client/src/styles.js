afdd2f6e0a5fd998953cc88badb3b91561c80df4
nihitgupta1 <nihitgupta619@gmail.com>
2022-07-08 01:24:26 +0530

    add client

---
+++ client/src/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    //
+    margin: "30px 0px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    // by me
+    width: "60%"
+  },
+  heading: {
+    
+    color: "rgba(0,183,255, 1)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
